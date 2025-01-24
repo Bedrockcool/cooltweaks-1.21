af8a390bb261445b6e1399cd827738b40d2fb647
Zed Merrick <bedrockdude@outlook.com>
2025-01-24 22:08:05 +0000

    Initial commit

---
+++ src/main/java/net/bedrock/cooltweaks/CoolTweaksclient.java
@@ -0,0 +1,10 @@
+package net.bedrock.cooltweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoolTweaksclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
